ecf182f5eec7f457f1ebde67ee03d23124ded336
dhruv0456 <dhruvpatodia801@gmail.com>
2024-11-27 10:02:22 +0530

    project_simple_grade

--- if-else2.cpp
+++ if-else2.cpp
@@ -39,13 +39,17 @@ int main()
         
         if (score >= 90) {
             grade = 'A';
-        } else if (score >= 80) {
+        }
+         else if (score >= 80) {
             grade = 'B';
-        } else if (score >= 70) {
+        }
+         else if (score >= 70) {
             grade = 'C';
-        } else if (score >= 60) {
+        }
+         else if (score >= 60) {
             grade = 'D';
-        } else {
+        }
+         else {
             grade = 'F';
         }
 
