a80a46e52a627b3a78c59c85bd236d05291267f2
dhruv0456 <dhruvpatodia801@gmail.com>
2024-12-06 10:44:26 +0530

    pattern_2

--- patterns_2.cpp
+++ patterns_2.cpp
@@ -1,23 +1,66 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-    int rows;
+int main()
+{
+    
+cout << "---------------------------------------------------" << endl;
+cout << "Q-1" << endl;
+     
+    
+    int n=5;  
 
-    cout << "Enter the number of rows: ";
-    cin >> rows;
-
-    for (int i = rows; i >= 1; i--) 
+    for (int i=0; i<n; i++) 
     {
-        for (int j = i; j >= 1; j--)
+        for (int j=n; j>i; j--) 
         {
             cout << j << " ";
         }
         cout << endl;
     }
 
-    
 
+cout << "---------------------------------------------------" << endl;
+cout << "Q-2" << endl;
+
+
+    int m = 5; 
+
+    for (int i=0; i<m; i++) 
+    {
+        for (int j=1; j<=m - i; j++) {
+            cout << j << " ";
+        }
+
+        for (int j=1; j<2 * i; j++) {
+            cout << "  "; 
+        }
+
+        for (int j=m - i; j>0; j--) {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+
+cout << "---------------------------------------------------" << endl;
+cout << "Q-3" << endl;
+
+
+
+    for (int i=5; i>=1; i--) {
+        for (int j=1; j<=i; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i=2; i<=5; i++) {
+        for (int j=1; j<=i; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
 
 }
